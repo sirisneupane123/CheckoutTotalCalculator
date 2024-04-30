124bd11b46070d909a0c5357ca92226f0e4cdae0
SirisNeupane <58123410+SirisNeupane@users.noreply.github.com>
2024-04-30 13:39:47 -0400

    Add test case for Idaho with standard sales tax on general items.

--- TotalCalculatorTest.py
+++ TotalCalculatorTest.py
@@ -12,5 +12,13 @@ class TestCheckoutSystem(unittest.TestCase):
         items = [{'type': 'everything else', 'price': 100.00}]
         self.assertAlmostEqual(calculate_total('MT', items), 100.00)
 
+    def test_normal_tax_id(self):
+        """
+        Test items purchased in Idaho with the standard tax rate applied.
+        """
+        items = [{'type': 'everything else', 'price': 100.00}]
+        self.assertAlmostEqual(calculate_total('ID', items), 106.00)
+
+
 if __name__ == '__main__':
     unittest.main()
